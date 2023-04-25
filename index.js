74819701e7d947c9e72f82c73c3c78f1753906b0
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-25 21:44:39 +0530

    handle array and object in swagger docs

--- index.js
+++ index.js
@@ -9,8 +9,34 @@ const file = fs.readFileSync("./swagger.yaml", "utf8");
 const swaggerDocument = YAML.parse(file);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/api/v1/", (_req, res) => {
+let courses = [
+  {
+    id: "11",
+    name: "Learn Reactjs",
+    price: 299,
+  },
+  {
+    id: "22",
+    name: "Learn Angular",
+    price: 399,
+  },
+  {
+    id: "33",
+    name: "Learn Django",
+    price: 499,
+  },
+];
+
+app.get("/api/v1/", (req, res) => {
   res.send("Hello from backend");
+});
+
+app.get("/api/v1/courseObject", (req, res) => {
+  res.send({ id: "11", name: "Master backend with node", price: 499 });
+});
+
+app.get("/api/v1/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(8080, () => {
